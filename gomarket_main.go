33f22b0d7e8c3cb6438c13eaa2aa679f80b2376f
Gabriel Woojae Lim <dalpengholic@gmail.com>
2021-08-11 12:48:20 +0300

    210811_3 edit main.go to test

--- gomarket_main.go
+++ gomarket_main.go
@@ -11,7 +11,8 @@ import (
 func main(){
 	// Create items
 	fruit1 := fruit.NewFruitItem("banana", 0.3)
-	fruit2 := fruit.NewFruitItem("apple", 0.2)
+	fruit2 := fruit.NewFruitItem("apple", 0.28)
+	fruit3 := fruit.NewFruitItem("Orange", 0.9)
 	fmt.Println(fruit1)
 	fmt.Println(unsafe.Sizeof(fruit1))
 
@@ -39,12 +40,15 @@ func main(){
 	// result2 := biobag1.Remove()
 	// fmt.Println(result2, biobag1)
 	result3 := biobag1.Add(&fruit2)
+	result4 := biobag1.Add(&fruit3)
+
 	fmt.Println(result3, biobag1, fruit2)
 	biobag1.List[0].Name = "Orange"
 	fmt.Println(*biobag1.List[0])
 
-
+    fmt.Println(result4)
     fmt.Println("현재 비닐에 있는 아이템:", biobag1)
+	fmt.Println("아이템들: ", biobag1.List[0], biobag1.List[1], biobag1.List[2])
 	weight3 := scale1.ShowWeight(biobag1)
 	fmt.Println(weight3)
 
