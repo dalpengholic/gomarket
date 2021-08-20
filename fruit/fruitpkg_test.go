bebca342d07ca34048391194b1c28a4cb7985554
Gabriel Woojae Lim <dalpengholic@gmail.com>
2021-08-20 11:45:30 +0300

    210820-2: Add test code

---
+++ fruit/fruitpkg_test.go
@@ -0,0 +1,15 @@
+package fruit_test
+import "testing"
+import "gomarket/fruit"
+
+func TestNewFruitItem(t *testing.T){
+	result1 := fruit.NewFruitItem("apple", 0.7)
+	if result1.Name != "apple" {
+		t.Errorf("결과1 값은 banana 이어야 합니다.")
+	}
+
+	result2 := fruit.NewFruitItem("banana", 0.7)
+	if result2.Weight != 0.7 {
+		t.Errorf("결과2 값은 0.7 이어야 합니다")
+	}
+}
