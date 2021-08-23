743d44c1a41a1302fc7092ebebfbbcd57a53cf90
Gabriel Woojae Lim <dalpengholic@gmail.com>
2021-08-23 15:36:24 +0300

    210823-2 Add gitignore

--- scale/scalepkg_test.go
+++ scale/scalepkg_test.go
@@ -24,15 +24,22 @@ func TestShowWeight(t *testing.T) {
 	testItem1 := fruit.NewFruitItem("banana", 0.95)
 	testItem2 := fruit.NewFruitItem("apple", 0.52)
 	testItem3 := biobag.NewBioBag()
+	testItem4 := biobag.NewBioBag()
 	testItem3.Add(&testItem1)
 	testItem3.Add(&testItem2)
 	result1 := scale.ShowWeight(testItem1)
 	result2 := scale.ShowWeight(testItem3)
+	result3 := scale.ShowWeight(testItem4)
 	if result1 != 0.95 {
 		t.Errorf("결과는 0.95이어야 합니다.")
 	}
 	if result2 != 1.47 {
 		t.Errorf("결과는 1.47이어야 합니다.")
 	}
+	if result3 != 0.0 {
+		t.Errorf("결과는 0.0 이어야 합니다.")
+	}
+
+
 
 }
